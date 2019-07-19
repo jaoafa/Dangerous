130a89fe9e32be516ac0796827f90560430bbe2d
Tomachi[ICHIGO] <lego@aol.jp>
2019-07-19 21:31:01 +0900

    add: 画像等添付ファイル対応

--- src/main/java/com/jaoafa/Dangerous/Discord/Event_ServerChatMessage.java
+++ src/main/java/com/jaoafa/Dangerous/Discord/Event_ServerChatMessage.java
@@ -1,5 +1,6 @@
 package com.jaoafa.Dangerous.Discord;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,7 @@ import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedE
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IMessage.Attachment;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.RequestBuffer;
 
@@ -38,6 +40,14 @@ public class Event_ServerChatMessage {
 		}
 		Bukkit.broadcastMessage(ChatColor.AQUA + "[" + guild.getName() + "#" + channel.getName() + "] " + ChatColor.RESET + name + ": " + formatcontent);
 
+		if(!message.getAttachments().isEmpty()){
+			List<String> urls = new ArrayList<>();
+			for(Attachment attachment : message.getAttachments()){
+				urls.add(attachment.getUrl());
+			}
+			Bukkit.broadcastMessage(ChatColor.AQUA + "[" + guild.getName() + "#" + channel.getName() + "] " + ChatColor.RESET + name + ": " + String.join(" ", urls));
+		}
+
 		content = content.replaceAll("@here", "");
 		content = content.replaceAll("@everyone", "");
 
@@ -49,8 +59,21 @@ public class Event_ServerChatMessage {
 				if(_name == null){
 					_name = author.getName();
 				}
-				_channel.sendMessage("**[" + guild.getName() + "#" + channel.getName() + "] " + _name + "**: " + _content);
+				_channel.sendMessage("**[" + guild.getName() + "#" + channel.getName() + "] " + _name + "**: " + _content + "\n");
 			});
+			if(!message.getAttachments().isEmpty()){
+				RequestBuffer.request(() -> {
+					String _name = author.getNicknameForGuild(guild);
+					if(_name == null){
+						_name = author.getName();
+					}
+					List<String> urls = new ArrayList<>();
+					for(Attachment attachment : message.getAttachments()){
+						urls.add(attachment.getUrl());
+					}
+					_channel.sendMessage("**[" + guild.getName() + "#" + channel.getName() + "] " + _name + "**: " + String.join(" ", urls));
+				});
+			}
 		}
 	}
 }
